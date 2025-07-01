0040b2b7ebd73dd27b5f825927fe4e999d55c0ff
luckhee <dcs1206@naver.com>
2025-07-01 12:28:03 +0900

    step-2

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <>
+      <h1>
+        명언
+        <ul>
+          <li>명언1</li>
+          <li>명언2</li>
+        </ul>
+      </h1>
+    </>
+  );
+}
